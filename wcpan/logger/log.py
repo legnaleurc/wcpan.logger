d66d2ac6ace7fae5dc9b2e901e20e76ca699d9d8
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-03-14 23:19:47 +0900

    define maximum length

--- wcpan/logger/log.py
+++ wcpan/logger/log.py
@@ -50,7 +50,7 @@ def EXCEPTION(name: str, exception: Exception = None) -> Logger:
 def setup(log_name_list: Iterable[str],
           file_path: str = None) -> List[logging.Logger]:
     formatter = logging.Formatter(
-        '{asctime}|{threadName:_<10.10}|{levelname:_<8}|{name}|{message}',
+        '{asctime}|{threadName:_<10.10}|{levelname:_<8}|{name:_<8.8}|{message}',
         style='{')
     handler = create_handler(file_path, formatter)
     loggers = [create_logger(name, handler) for name in log_name_list]
